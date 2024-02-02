a3cace80d94245853bc748a8643bef685d3e3554
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-02-03 02:25:56 +0530

    Sign Up Backend Done

---
+++ src/main/java/com/inn/library/constents/LibraryConstants.java
@@ -0,0 +1,6 @@
+package com.inn.library.constents;
+
+public class LibraryConstants {
+    public static final String SOMETHING_WENT_WRONG="Something went wrong.";
+    public static final String INVALID_DATA="Invalid Data.";
+}
