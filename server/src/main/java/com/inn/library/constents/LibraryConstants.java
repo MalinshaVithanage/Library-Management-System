368782d57807d22c2d2912cded24548a7c795aa8
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-02-04 00:53:34 +0530

    All Set Up Done

--- server/src/main/java/com/inn/library/constents/LibraryConstants.java
+++
@@ -1,6 +0,0 @@
-package com.inn.library.constents;
-
-public class LibraryConstants {
-    public static final String SOMETHING_WENT_WRONG="Something went wrong.";
-    public static final String INVALID_DATA="Invalid Data.";
-}
