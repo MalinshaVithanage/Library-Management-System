61565d6d0fd0fe1bdb5947e3d4e2008b24afea51
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-02-04 02:06:26 +0530

    Done- Sign Up Backend

---
+++ server/src/main/java/com/inn/library/services/auth/AuthServiceImpl.java
@@ -0,0 +1,33 @@
+package com.inn.library.services.auth;
+
+import com.inn.library.dto.SignupRequest;
+import com.inn.library.dto.UserDto;
+import com.inn.library.entity.User;
+import com.inn.library.enums.UserRole;
+import com.inn.library.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService{
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDto createCustomer(SignupRequest signupRequest) {
+        User user=new User();
+        user.setName(signupRequest.getName());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(signupRequest.getPassword());
+        user.setUserRole(UserRole.CUSTOMER);
+        User createdUser = userRepository.save(user);
+        UserDto userDto = new UserDto();
+        userDto.setId(createdUser.getId());
+        return userDto;
+    }
+
+    @Override
+    public boolean hasCustomerWithEmail(String email) {
+        return userRepository.findFirstByEmail(email).isPresent();
+    }
+}
