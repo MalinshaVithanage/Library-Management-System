559171882d166c8843cb2e8aba37b60ec69194f9
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-02-06 00:30:47 +0530

    Done Admin

--- client/src/app/modules/admin/admin-routing.module.ts
+++ client/src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { PostProductComponent } from './components/post-product/post-product.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component: AdminDashboardComponent},
+  {path:'category',component: AddCategoryComponent},
+  {path:':categoryId/product', component: PostProductComponent},
+  {path:':categoryId/products', component: ViewProductsComponent},
+  {path: 'product/:productId', component: UpdateProductComponent},
+  {path:'**', redirectTo: 'dashboard'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzButtonModule, NzButtonSize } from 'ng-zorro-antd/button';
+import { AdminService } from '../../services/admin.service';
+import { DemoNgZorroAntdModule } from '../../../../../NgZorroImportsModule';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DemoNgZorroAntdModule,CommonModule, NzButtonModule, RouterModule,
+     NzGridModule, NzFormModule ,NzInputModule,ReactiveFormsModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
+  constructor(private service: AdminService, private fb:FormBuilder) {}
+  categories: any=[];
+  validateForm!:FormGroup;
+  size:NzButtonSize = 'large'; // esto es para que el boton sea grande
+  isSpinnerVisible?:boolean;
 
+
+
+  ngOnInit(): void {
+    
+    this.validateForm = this.fb.group({
+      title: ['', [Validators.required]],
+    });
+    this.getCategories();
+  }
+
+    
+    
+
+  getCategories() {
+    this.service.getAllCategories().subscribe((res) => {
+      res.forEach((element:any) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.returnedImg ;
+        this.categories.push(element);
+      });
+      console.log('Categorias obtenidas con imagenes procesadas: \n');
+      console.log(this.categories);
+    });
+  };
+
+
+  submitForm() {
+    this.isSpinnerVisible = true;
+    this.categories = [];
+    this.service.getAllCategoriesByTitle(this.validateForm.get(['title'])?.value).subscribe((res) => {
+      res.forEach((element:any) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.returnedImg ;
+        this.categories.push(element);
+        
+      });
+      this.isSpinnerVisible = false;
+      console.log('Categorias obtenidas por busquedad con imagenes procesadas: \n');
+      console.log(this.categories);
+    });
+  }
 }
