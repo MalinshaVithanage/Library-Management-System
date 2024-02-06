fb70c0f97b4c690aa999f2e212509b072723305d
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-02-06 15:49:46 +0530

    Library Management System All Finished

--- client/src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
+++ client/src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
+import { NzButtonSize } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+//import { Router, RouterOutlet } from '@angular/router';  esto es para que funcione el router-outlet en el app.component.html
+import { FormBuilder,ReactiveFormsModule, FormsModule, Validators, FormGroup } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { RouterModule } from '@angular/router';
 
+import { NzInputModule } from 'ng-zorro-antd/input';
 @Component({
   selector: 'app-customer-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NzGridModule, CommonModule, NzButtonModule, NzFormModule, NzInputModule, RouterModule, ReactiveFormsModule, FormsModule],
   templateUrl: './customer-dashboard.component.html',
   styleUrl: './customer-dashboard.component.scss'
 })
 export class CustomerDashboardComponent {
+  constructor(private service: CustomerService, private fb:FormBuilder) {}
+  categories: any=[];
+  validateForm!:FormGroup;
+  size:NzButtonSize = 'large'; // esto es para que el boton sea grande
+  isSpinnerVisible?:boolean;
+
+
+
+  ngOnInit(): void {
+    
+    this.validateForm = this.fb.group({
+      title: ['', [Validators.required]],
+    });
+    this.getCategories();
+  }
+
+    
+    
+
+  getCategories() {
+    this.service.getAllCategories().subscribe((res) => {
+      res.forEach((element:any) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.returnedImg ;
+        this.categories.push(element);
+      });
+      console.log('Categorias obtenidas con imagenes procesadas: \n');
+      console.log(this.categories);
+    });
+  };
+
+
+  submitForm() {
+    this.isSpinnerVisible = true;
+    this.categories = [];
+    this.service.getAllCategoriesByTitle(this.validateForm.get(['title'])?.value).subscribe((res) => {
+      res.forEach((element:any) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.returnedImg ;
+        this.categories.push(element);
+        
+      });
+      this.isSpinnerVisible = false;
+      console.log('Categorias obtenidas por busquedad con imagenes procesadas: \n');
+      console.log(this.categories);
+    });
+  }
 
 }
--- client/src/app/modules/customer/customer-routing.module.ts
+++ client/src/app/modules/customer/customer-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewProductsByCategoryComponent } from './components/view-products-by-category/view-products-by-category.component';
+import { PostReservationComponent } from './components/post-reservation/post-reservation.component';
+import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component: CustomerDashboardComponent},
+  {path:':categoryId/products', component: ViewProductsByCategoryComponent},
+  {path:'reservation',component: PostReservationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
